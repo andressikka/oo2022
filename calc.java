45464b73ebad6c9b37198b49704a43f754dc7b4a
Andres <andres.sikka98@gmail.com>
2022-02-14 10:25:13 +0200

    First upload

--- calc.java
+++ calc.java
@@ -1,27 +1,33 @@
-public class clac{
+public class calc{
     public static void main(String args[]){
-        String testVar = args[0];
         int length = args.length;
         double[] valueArray = new double [length];
-        for (int i = 1; i < length; i++){
-            valueArray[i] = Double.parseDouble(args[i]);
+
+        for (int i = 1; i < length; i++)
+        {
             System.out.println(args[i]);
+            valueArray[i] = Double.parseDouble(args[i]);
         }
 
         System.out.println(args[0].getClass().getSimpleName());
         System.out.println("test");
         
-        if(testVar == "+"){
+        if(args[0].equals("plus")){
             System.out.println("test plus");
             System.out.println(plus(valueArray[1], valueArray[2]));
+        } 
+        else if (args[0].equals("minus")) 
+        {
+            System.out.println(minus(valueArray[1], valueArray[2]));
         }
-        // } else if (args[0] == "-"){
-        //     System.out.println(minus(valueArray[1], valueArray[2]));
-        // } else if (args[0] == "*"){
-        //     System.out.println(multiply(valueArray[1], valueArray[2]));
-        // } else if (args[0] == "/"){
-        //     System.out.println(divide(valueArray[1], valueArray[2]));
-        // }
+        else if (args[0].equals("multiply"))
+        {
+            System.out.println(multiply(valueArray[1], valueArray[2]));
+        }
+        else if (args[0].equals("divide")) 
+        {    
+            System.out.println(divide(valueArray[1], valueArray[2]));
+        }
     }
     
     public static double plus(double a, double b){
@@ -35,9 +41,5 @@ public class clac{
     }
     public static double divide(double a, double b){
         return a / b;
-    }
-    
-    public static String stringAnalysis(String calculation){
-        return "";
     }
 }    
